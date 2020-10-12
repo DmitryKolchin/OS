4bc888ea28336f6c14b4c7b14f278c103db88496
ZHABA <284594@niuitmo.ru>
2020-10-12 17:48:15 +0300

    Special task added

--- Lab1/x.sh
+++ Lab1/x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-man bash | grep -i -o  "[a-zA-Z]\{4\}" | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -r -n | head --lines=3
+man bash | grep -i -o  "[a-zA-Z]\{4\}[a-zA-Z]*" | tr "[:upper:]" "[:lower:]" | sort | uniq -c | sort -r -n | head --lines=3
 
 
 
